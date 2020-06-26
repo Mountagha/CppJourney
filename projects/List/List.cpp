0af711359d744b47e8e8f3184c79b44ef5e98a3a
Mountagha <muntaghaba@gmail.com>
2020-06-26 04:25:31 -0400

    Adding iterator to List

--- projects/List/List.cpp
+++ projects/List/List.cpp
@@ -1,94 +1,116 @@
 #include "List.h"
 
-
-template<typename T>
-Link<T>* Link<T>::insert(Link* n){  // insert n before this object
-    if(n == nullptr) return this; //nothing to insert
-    //if(this == nullptr) return n; // nothing to insert into;
-    n->succ = this;
-    if(prev) prev->succ = n;
-    n->prev = prev;
-    prev = n;
-    return n;
-}
-
-template<typename T>
-Link<T>* Link<T>::add(Link* n) { // insert n after this object
-    if(n==nullptr) return this;
-    n->succ = succ;
-    n->prev = this;
-    if(succ) succ->prev = n;
-    this->succ = n;
-    return n;
-}
-
-template<typename T>
-Link<T>* Link<T>::erase(){  // erase this from list return succ (or the prev if the last elem)
-    if(succ) succ->prev = prev;
-    if(prev) prev->succ = succ;
-    return succ;
-}
-
-template<typename T>
-Link<T>* Link<T>::find(const T& s) {
-    Link* p = this;
-    while(p){
-        if(p->value == s) return p;
-        p=p->succ;
-    }
-    return nullptr;
-}
-
-template<typename T>
-Link<T>* Link<T>::advance(int n) {
-    Link* p = this;
-    //  positive n moves forward, negative backward
-    if(0<n) {
-        while(n--){
-            if(p->succ == nullptr) return nullptr;
-            p = p->succ;
+template<class Elem>
+class List<Elem>::iterator{
+    public:    
+        Link<Elem>* curr;
+        iterator(Link<Elem>* p): curr{p} {}
+        iterator& operator++() { curr = curr->succ; return *this; } // forward
+        iterator& operator--() { curr = curr->prev; return *this; } // backward
+        Elem& operator*() { return curr->val; } // deferencement
+        friend bool operator==(const iterator& a, const iterator& b){
+            return a.curr == b.curr;
         }
-    }
-    else if(n < 0){
-        while(n++){
-            if(p->prev == nullptr) return nullptr;
-            p = p->prev;
+        friend bool operator!=(const iterator& a, const iterator& b){
+            return a.curr != b.curr;
         }
-    }
-    return p;
 
+};
+
+template<class Elem>
+typename List<Elem>::iterator List<Elem>::begin(){
+    return iterator(first);
 }
 
-template<typename T>
-void print_all(Link<T>* p){
-    while(p->previous()) p = p->previous(); // go back to the beginning of the list
-    while (p)
-    {
-        cout << p->value << " ";
-        p = p->next();
-    }
-    cout << "\n";
+template<class Elem>
+typename List<Elem>::iterator List<Elem>::end(){
+    return iterator(last);
 }
+
+template<class Elem>
+typename List<Elem>::iterator List<Elem>::insert(iterator p, const Elem& v){  // insert v before p
+    cout << "aussi" << "\n";
+    Link<Elem>* new_elem = new Link<Elem>(v, p.curr->prev, p.curr);
+    p.curr->prev = new_elem;
+
+    if(p.curr->prev) p.curr->prev->succ = new_elem;
+    if(p.curr == first){
+        cout << "yes: " << *p << "\n";
+        --p;
+        first = p.curr;
+        return p;
+    } 
+    return --p;             // return inserted elem
+}
+
+
+template<class Elem>
+typename List<Elem>::iterator List<Elem>::erase(iterator p){  // remove p from the list
+    if(p.curr->prev) p.curr->prev->succ = p.curr->succ;
+    if(p.curr->succ) p.curr->succ->prev = p.curr->prev;
+    auto new_iter = ++p;
+    delete p;
+    return new_iter;
+}
+
+template<class Elem>
+void List<Elem>::push_back(const Elem& v){
+    Link<Elem>* new_elem = new Link<Elem>(v, last->prev, last);
+    last->prev->succ = new_elem;
+    last->prev = new_elem;
+}
+
+template<class Elem>
+void List<Elem>::push_front(const Elem& v){
+    first = new Link<Elem>(v, nullptr, first);
+}
+
+template<class Elem>
+void List<Elem>::pop_back(){
+    auto elem_to_pop = last->prev;
+    last->prev->prev->succ = last;
+    last->prev = last->prev->prev;
+    delete elem_to_pop;
+}
+
+template<class Elem>
+void List<Elem>::pop_front(){
+    auto elem_to_pop = first;
+    first = first->succ;
+    delete elem_to_pop;
+}
+
+template<class Elem>
+Elem& List<Elem>::front(){
+    return first->val;
+}
+
+template<class Elem>
+Elem& List<Elem>::back(){
+    return last->prev->val;
+}
+
 
 // To avoid the template linker error
 template class Link<string>;
 
 
 int main(){
-    Link<string>* l = new Link<string>("B");
-    l = l->insert(new Link<string>("A"));
-    l = l->find("B");
-    l = l->add(new Link<string>("C"));
-    l = l->add(new Link<string>("E"));
-    print_all(l);
-    l = l->advance(-1);
-    l = l->add(new Link<string>("D"));
-    l = l->advance(1);
-    l = l->add(new Link<string>("F"));
-    print_all(l);
-    l = l->advance(-2);
-    l = l->erase();
-    print_all(l);
+    List<string> list;
+    cout << (list.begin() == list.end()) << "\n";
+    auto it = list.insert(list.begin(), "tete");
+    cout << (list.begin() == list.end()) << "\n";
+    cout << list.front() << "\n";
+    cout << list.back() << "\n";
+    //it = list.insert(it, "tete1");
+    //it = list.insert(it, "tete2");
+    cout << *it << "\n";
+    list.push_back("other_end");
+    //list.push_front("other_front");
+    //List<string>::iterator it3 = list.begin();
+    
+    //for(auto it = list.begin(); it != list.end(); ++it)
+    //    cout << *it << "\n";
 
     return 0;
 }
--- projects/List/List.h
+++ projects/List/List.h
@@ -3,25 +3,40 @@
 
 using namespace std;
 
-template<typename T>
-class Link {
-    public:
-        T value;
-        Link(const T& v, Link* p=nullptr, Link* s=nullptr): value{v}, prev{p}, succ{s} {}
-        Link* insert(Link* n); // insert n before this object
-        Link* add(Link* n);   // insert n after this object
-        Link* erase();       // erase this object from the list
-        Link* find(const T& v); // find s in the const list
+template<typename Elem>
+struct Link{
+    Link* prev;
+    Link* succ;
+    Elem val;
+    Link(const Elem v=Elem(), Link* p = nullptr, Link* s=nullptr): val{v}, prev{p}, succ{s} {}
 
-        Link* advance(int n); // move n positions in list
-
-        Link* next() const { return succ; }; 
-        Link* previous() const { return prev; }
-    
-    private:
-        Link* prev;
-        Link* succ;
 };
+
+template<typename Elem>
+class List {
+    public:
+        
+        class iterator; // member type: iterator
+
+        iterator begin(); // iterator to the first element
+        iterator end(); // iterator to one beyond the last element
+
+        List(): first{new Link<Elem>()}, last{first} {}
+        iterator insert(iterator p, const Elem& v); // insert v into list before p
+        iterator erase(iterator p);       // remove p from the list
+
+        void push_back(const Elem& v); // insert v at the end
+        void push_front(const Elem& v); // insert v at the front
+        void pop_back();                // remove the first element
+        void pop_front();               // remove the last element
+
+        Elem& front();                  // the first element
+        Elem& back();                   // the last element
+
+        Link<Elem>* first;
+        Link<Elem>* last;
+};
+
 
 template<typename T>
 void print_all(Link<T>* p);

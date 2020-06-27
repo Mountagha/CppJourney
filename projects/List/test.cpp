abb7b3a71a5e3001dae51409e191fca134ced65e
Mountagha <muntaghaba@gmail.com>
2020-06-27 00:36:34 -0400

    iterator added to list

--- projects/List/test.cpp
+++ projects/List/test.cpp
@@ -1,20 +1,6 @@
-#include "List.h"
+#include <iostream>
 
 int main(){
-    Link<string>* l = new Link<string>("B");
-    l = l->insert(new Link<string>("A"));
-    l = l->find("B");
-    l = l->add(new Link<string>("C"));
-    l = l->add(new Link<string>("E"));
-    print_all(l);
-    l = l->advance(-1);
-    l = l->add(new Link<string>("D"));
-    l = l->advance(1);
-    l = l->add(new Link<string>("F"));
-    print_all(l);
-    l = l->advance(-2);
-    l = l->erase();
-    print_all(l);
-
+    // All tests included in List.cpp for template rendering issues
     return 0;
 }
